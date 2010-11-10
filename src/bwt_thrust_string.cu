bba0da716acec80b9e42415abee46b093778d6f7
anushree <anushree@anushree-laptop.(none)>
2010-11-10 22:51:01 +0530

    Keys are sorting but values aren't.

--- src/bwt_thrust_string.cu
+++ src/bwt_thrust_string.cu
@@ -22,7 +22,6 @@ return string[value[tid]-1]
 #include <stdio.h>
 #include <iostream>
 #include <cstring>
-#include <string.h>
 
 #define ARGC_EXPECTED_VAL 2
 
@@ -33,56 +32,52 @@ __global__ void fnKern(char *key, int *val, char *str)
 
 int main(int argc, char *argv[])
 {
-	if (argc != ARGC_EXPECTED_VAL)
+	/*if (argc != ARGC_EXPECTED_VAL)
 	{
 		std::cout << "Usage: sort_thrust <string to be sorted>\n";
 		return 1;
-	}
-	
-	int N = strlen(argv[1]);
-	
-	char *key, *str;
-	int *val;
-	char *strH = new(char);	
-	int *value = new(int);
+	}*/
+	char *word = new(char);
+	std::cin >> word;
+	int N = strlen(word);
 
-	strH[0] = argv[1][0];
+	char *strH = new(char);	
+	int *value = NULL;
+	value = new int[N];
+	
+	strH[0] = word[0];
 	value[0] = 0;
 	for (int i = 1; i < N; i++)
 	{
-		strH[i] = argv[1][N - i -1];
-		value[i] = N - i - 1;
+		strH[i] = word[N - i];
+		value[i] = N - i;
 	}
-	//strH = strrev(argv[1]);	
+
+	char *key, *str;
+	int *val;
 	
 	cudaMalloc((void**)&key, sizeof(char) * N);
 	cudaMalloc((void**)&val, sizeof(int) * N);
 	cudaMalloc((void**)&str, sizeof(char) * N);
-	
+
 	thrust::device_ptr<char> keyD(key);	
 	thrust::device_ptr<char> strD(str);
 	thrust::device_ptr<int> valD(val);
 	
-	thrust::copy(strH, strH + N - 1, keyD);
-	//thrust::copy(argv[1], argv[1],keyD);
-	//thrust::copy(strH, strH + N - 2, keyD + 1);	//The string to be sorted is taken from the command line.
-							//We originally had keyH.begin() here, but I did not see the need for a keyH at all
+	thrust::copy(strH, strH + N, keyD);
 
-	//thrust::sequence(valD, valD + N);	//set valD's values to a sequence from 0 to N-1
-	thrust::copy(value, value + N - 1, valD);
+	thrust::copy(value, value + N, valD);
 	
-	thrust::stable_sort_by_key(keyD, keyD + N - 1, valD);
+	thrust::stable_sort_by_key(keyD, keyD + N, valD);
 	
-	//thrust::copy(valD.begin(), valD.end(), valH.begin());	//Copy sorted values to host
-	//thrust::copy(valH.begin(), valH.end(), valD.begin());	//Copy sorted values to device
-	//Whuuut are these two lines??? Table tennis? :P
+//	fnKern<<<1, N>>>(key, val, str);
 	
-	fnKern<<<1, N>>>(key, val, str);
-	
-	cudaMemcpy(strH, str, N, cudaMemcpyDeviceToHost);
+	cudaMemcpy(strH, key, N, cudaMemcpyDeviceToHost);
+	cudaMemcpy(value, val, N, cudaMemcpyDeviceToHost);
 //	strH = thrust::raw_pointer_cast(strD);	
+	std::cout <<strH << std::endl;
 	for (int i = 0; i < N; i++)
-	std::cout << strH[i] << std::endl;
+	std::cout << value[i] << std::endl;
 
 	cudaFree(key);
 	cudaFree(str);
