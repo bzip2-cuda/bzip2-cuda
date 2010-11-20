c7f43c66a3fe89911dcb27831ca8757999a04355
Anirudh Ramachandran <anirudhr1989@gmail.com>
2010-11-21 01:03:58 +0530

    Started writing the string_sort header and source to be included

---
+++ lib/string_sort.cu
@@ -0,0 +1,97 @@
+#ifndef STRING_SORT_CU
+
+//Taken from https://groups.google.com/group/thrust-users/msg/0eac80d2e41cbcfb?pli=1, https://groups.google.com/group/thrust-users/browse_thread/thread/f4b1b825cc927df9?pli=1, http://ldn.linuxfoundation.org/article/c-gpu-and-thrust-strings-gpu
+
+//Our thanks to Shashank LASTNAME
+
+#include <cstring>
+#include <string>
+#include <vector>
+#include <iterator>
+
+#include <thrust/device_ptr.h>
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/sort.h>
+#include <thrust/copy.h>
+
+#include "string_sort.h"
+
+#define POOL_SZ (10*1024*1024)
+
+using namespace std;
+__host__ static void device_string::init()
+{
+	static bool v = true;
+	if( v )
+	{
+		v = false;
+
+		pool_cstr = thrust::device_malloc(POOL_SZ);
+		pool_raw  = (char*)raw_pointer_cast( pool_cstr );
+		pool_top = pool_cstr;
+	}
+}
+// Destructor for device variables used.
+__host__ static void device_string::fin()
+{
+	init();
+	thrust::device_free(pool_cstr);
+}
+
+// Parametrized constructor to copy one device_string to another.
+__host__ device_string::device_string( const device_string& s )
+{
+	cstr_len = s.cstr_len;
+	raw = s.raw;
+	cstr = s.cstr;
+}
+
+// Parametrized constructor to copy a std::string to device_string type
+__host__ device_string::device_string( const std::string& s )
+{
+	cstr_len = s.length();
+	init();
+	cstr = pool_top;
+	pool_top += cstr_len+1;
+	raw = (char *) raw_pointer_cast(cstr);
+	cudaMemcpy( raw, s.c_str(), cstr_len+1, cudaMemcpyHostToDevice );
+}
+
+// Default constructor.
+__host__ __device__ device_string::device_string()
+{
+	cstr_len = -1;
+	raw = NULL;
+}
+
+// Conversion operator to copy device_string type to std::string
+
+__host__ device_string::operator std::string(void) //IS THIS SYNTAX RIGHT?
+{
+	std::string ret;
+	//device_ptr<char*>::iterator it = cstr.begin();
+	thrust::copy(cstr, cstr+cstr_len, back_inserter(ret));
+	return ret;
+}
+
+//SHOULD THIS BE COMMENTED OUT?
+char* device_string::pool_raw;
+thrust::device_ptr<char> device_string::pool_cstr;
+thrust::device_ptr<char> device_string::pool_top;
+
+// User-defined comparison operator
+bool __device__ operator< (device_string lhs, device_string rhs)
+{
+	char *l = lhs.raw;
+	char *r = rhs.raw;
+
+	for( ; *l && *r && *l==*r; )
+	{
+	++l;
+	++r;
+	}
+	return *l < *r;
+}
+
+#endif
