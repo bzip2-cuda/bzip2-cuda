7991c2177e10075e847f2b8f0c4249c9ebee2d96
Anirudh Ramachandran <anirudhr1989@gmail.com>
2011-01-13 10:02:41 +0530

    Started Huffman

---
+++ src/huffman.cu
@@ -0,0 +1,19 @@
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/device_ptr.h>
+#include <thrust/copy.h>
+#include <thrust/sequence.h>
+#include <thrust/sort.h>
+#include <thrust/find.h>
+
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+}
