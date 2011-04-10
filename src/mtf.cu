78ee95dec9aa48796c614795d46dcd85ce957c9a
anushree <anushree@anushree-laptop.(none)>
2011-04-10 18:00:03 +0530

    String being passed to the function, error in 50

--- src/mtf.cu
+++ src/mtf.cu
@@ -28,28 +28,30 @@ void mtf(vector<char> word)
 	thrust::host_vector<char> list(256);
 	thrust::device_vector<char> d_word(word.size());
 	thrust::device_vector<int> dRes;
-	int counter, index;
+	int counter;
 	thrust::device_vector<char>::iterator iter;
-	thrust::copy(word.begin(), word.end(), d_word.begin());
-
+	thrust::host_vector<char> h_word(word.size());
+	char ch;
+/*	h_word = word;
+	d_word = h_word;
+	h_word = d_word;
+*/	thrust::copy(word.begin(), word.end(), d_word.begin());
+	thrust::device_vector<char> temp(256);
 	for (counter = 0; counter < word.size(); counter++)
 	{
 		//Scan for character on cpu
 		iter = thrust::find(d_list.begin(), d_list.end(), d_word[counter]);
-		//index = iter;
-//		fnSearch<<<1, 256>>>(d_list, d_word[counter], dRes);
 
-		//Shifting of the character set in parallel
-		thrust::device_vector<char> temp(256);
+		//Shifting of the character set in parallel		
 		thrust::copy(d_list.begin(), iter - 1, temp.begin());
 		thrust::copy(temp.begin(), iter - 1, d_list.begin() + 1);
 
 		d_list[0] = d_word[counter];
-	//	thrust::copy(d_list.begin(), d_list.end(), list.begin());
+		thrust::copy(d_list.begin(), d_list.end(), list.begin());
 	}
 	for (counter = 0; counter <= word.size(); counter++)
 	{
-		char ch = list[counter];		
+		ch = list[counter];		
 		cout << counter << "\t" << ch << endl;
 	}
 }
@@ -65,12 +67,11 @@ int main(int argc, char *argv[])
 	int len = strlen(argv[1]);
 	char *arg = new char(len);
 	strcpy(arg, argv[1]);
-	
-	vector<char> word(len);
-	while ( *(arg) != '\0' )
+	vector<char> word(arg, arg + sizeof(arg));
+	/*while ( *(arg) != '\0' )
 	{
 		word.push_back(*(arg++));
-	}
+	}*/
 	mtf(word);
 	return 0;
 }
